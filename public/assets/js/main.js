54c1bb234a58c2bf813366ad1ecc4f18c1eb4b53
Vorn-Hagen <thibaut.francois@edu.devinci.fr>
2023-07-17 15:28:17 +0200

    Checkpoint

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -3,5 +3,27 @@ console.log('Hello World')
 fetch('/api')
     .then(r=>r.json())
     .then(r=>{
-        document.querySelector('pre').innerHTML = JSON.stringify(r, null, 2)
+        //document.querySelector('pre').innerHTML = JSON.stringify(r, null, 2)
+        //document.querySelector('#test_img').src = r.champ_selected[0].img
+
+        let mainSection = document.querySelector('.select')
+
+        for (let y = 0; y <= mainSection.childElementCount; y++) {
+
+            function func(i){
+                mainSection.children[i].children[0].children[3].children[0].value = r.champ_selected[i].nom
+            }
+
+            function named(i) {
+                mainSection.children[i].children[0].children[1].children[0].src = r.champ_selected[i].img
+
+                func(i)
+            }
+
+            for(champ in r.champ_selected){
+                named(y)
+            }
+        }
+
+
     })
--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -36,12 +36,29 @@ class AppController extends AbstractController
             $table=[
                 'champ_selected'=> [
                     [
-                        'nom'=>'perso 1',
-                        'id'=>1,
+                        'nom'=>'Ahri',
+                        'img'=>'https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt1259276b6d1efa78/5db05fa86e8b0c6d038c5ca2/RiotX_ChampionList_ahri.jpg?quality=90&width=250',
+                        'role'=>'Carry AP'
                     ],
                     [
-                        'nom'=>'perso 2',
-                        'id'=>2,
+                        'nom'=>'Braum',
+                        'img'=>'https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/bltd140e30fa86d6ddd/5db05fb2242f426df132f95d/RiotX_ChampionList_braum.jpg?quality=90&width=250',
+                        'role'=>'Tank'
+                    ],
+                    [
+                        'nom'=>'Caitlyn',
+                        'img'=>'https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt014f4b6fc9bacd10/61b1eb901d158d4007de9685/RiotX_ChampionList_caitlyn_v2.jpg?quality=90&width=250',
+                        'role'=>'Carry AD'
+                    ],
+                    [
+                        'nom'=>'Dr. Mundo',
+                        'img'=>'https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blte88a3d7e9e408904/61b1ea136a78b87751002a68/RiotX_ChampionList_drmundo_v2.jpg?quality=90&width=250',
+                        'role'=>'Tank'
+                    ],
+                    [
+                        'nom'=>'Shaco',
+                        'img'=>'https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/bltc8b1d1ba926d01cc/5db060036e8b0c6d038c5cba/RiotX_ChampionList_shaco.jpg?quality=90&width=250',
+                        'role'=>'Assassin'
                     ],
                 ],
 
